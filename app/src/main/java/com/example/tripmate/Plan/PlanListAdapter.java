dc570005fd677cc449e9000dd84c1871f653e33a
jec1128 <abcabc4567@naver.com>
2019-11-13 23:04:33 +0900

    11.12 11:03

--- app/src/main/java/com/example/tripmate/Plan/PlanListAdapter.java
+++ app/src/main/java/com/example/tripmate/Plan/PlanListAdapter.java
@@ -1,26 +1,64 @@
 package com.example.tripmate.Plan;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.tripmate.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class PlanListAdapter extends RecyclerView.Adapter<PlanListAdapter.PlanListViewHolder> {
-    private static PlanListAdapter listAdapter = new PlanListAdapter();
+    private static PlanListAdapter listAdapter;
     private ArrayList<PlanListModel> planlist = new ArrayList<>();
+    private Context context;
 
-    public PlanListAdapter(ArrayList<PlanListModel> planlist){
+    public PlanListAdapter(Context context, ArrayList<PlanListModel> planlist){
+        this.context = context;
         this.planlist = planlist;
     }
 
     public PlanListAdapter() { }
 
-    public void get(){
+    //일정목록 가져오기
+    public void getList(){
+        //removeAllItem();
+        HttpPlanList httpPlanList = new HttpPlanList();
+        HttpPlanList.sendTask sendTask = httpPlanList.new sendTask();
+        String result = null;
+        try {
+            result = sendTask.execute("1").get(); //page번호 전송
+            JSONArray jarray = null;
+            jarray = new JSONObject(result).getJSONArray("addlist");
 
+            if (jarray != null) {
+                for(int i = 0; i < jarray.length(); i++) {
+                    JSONObject jsonObject = jarray.getJSONObject(i);
+                    String code = jsonObject.getString("plancode");
+                    String place = jsonObject.getString("place");
+                    String title = jsonObject.getString("title");
+                    String start = jsonObject.getString("start");
+                    String end = jsonObject.getString("end");
+                    PlanListModel model = new PlanListModel(code, place, title, start, end);
+                    planlist.add(model);
+                }
+            }
+        } catch (ExecutionException | InterruptedException | JSONException e) {
+            e.printStackTrace();
+        }
+        //notifyDataSetChanged();
     }
 
     @Override
@@ -31,16 +69,33 @@ public class PlanListAdapter extends RecyclerView.Adapter<PlanListAdapter.PlanLi
         return holder;
     }
 
+
     @Override
     public void onBindViewHolder(PlanListAdapter.PlanListViewHolder holder, int position) {
-        holder.txtPlace.setText(planlist.get(position).getPlanPlace());
-        holder.txtTitle.setText(planlist.get(position).getPlanTitle());
-        holder.txtStart.setText(planlist.get(position).getPlanStart());
-        holder.txtEnd.setText(planlist.get(position).getPlanEnd());
+        int cardposit = position;
+        final PlanListViewHolder planListViewHolder = (PlanListViewHolder) holder;
+        final String code = planlist.get(cardposit).getPlanCode();
+        planListViewHolder.txtPlace.setText(planlist.get(cardposit).getPlanPlace());
+        planListViewHolder.txtTitle.setText(planlist.get(cardposit).getPlanTitle());
+        planListViewHolder.txtStart.setText(planlist.get(cardposit).getPlanStart());
+        planListViewHolder.txtEnd.setText(planlist.get(cardposit).getPlanEnd());
+
+        planListViewHolder.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PlanTripActivity.class);
+                intent.putExtra("plancode", code);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 
-    public void addList(int position, PlanListModel model){
-        planlist.add(model);
+    public void addList(ArrayList <PlanListModel> planlist){
+        this.planlist.addAll(planlist);
+    }
+
+    public void removeAllItem() {
+        planlist.clear();
     }
 
     @Override
@@ -51,12 +106,15 @@ public class PlanListAdapter extends RecyclerView.Adapter<PlanListAdapter.PlanLi
 
     public class PlanListViewHolder extends RecyclerView.ViewHolder {
         public TextView txtPlace, txtStart, txtEnd, txtTitle;
+        public ImageButton imageButton;
         public PlanListViewHolder(View view) {
             super(view);
             txtPlace = (TextView)view.findViewById(R.id.tv_loc);
             txtStart = (TextView)view.findViewById(R.id.tv_startday);
             txtEnd = (TextView)view.findViewById(R.id.tv_endday);
             txtTitle = (TextView)view.findViewById(R.id.tv_title);
+            imageButton = (ImageButton)view.findViewById(R.id.img_tripshow);
+
         }
     }
 
