003ac8dfd7f1cbff1ba08ac46beda3020a498e01
jec1128 <abcabc4567@naver.com>
2019-11-14 01:47:43 +0900

    1

--- app/src/main/java/com/example/tripmate/Ip.java
+++ app/src/main/java/com/example/tripmate/Ip.java
@@ -1,8 +1,8 @@
 package com.example.tripmate;
 
 public class Ip {
-   //private static String ip = "122.199.81.61";
-    private static String ip = "192.168.214.146";
+    private static String ip = "122.199.81.61";
+   // private static String ip = "192.168.214.146";
     public Ip() { }
 
     public String getIP() {
